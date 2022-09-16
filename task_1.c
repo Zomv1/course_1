87c931c6216dbdedbce15c18cda4a7c88e236f58
AMZomb <50305192+AMZomb@users.noreply.github.com>
2022-09-16 10:59:40 +0300

    Update task_1.c

--- task_1.c
+++ task_1.c
@@ -2,19 +2,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
+#include <string.h>
 int main()
 {
     system("chcp 1251");
     double r, R, d;
+    string r_str, R_str;
     printf("Введите длину стороны квадрата - ");
-    scanf_s("%lf", &R);
+    scanf_s("%s", &R_str);
     printf("\nВведите радиус круга - ");
-    scanf_s("%lf", &r);
+    scanf_s("%s", &r_str);
     d = floor(10000*(R * sqrt(2)))/10000;
     printf("\n d = %lf", d);
     printf("\n\n");
+    if(ststr(R, "*sqrt("))
+        memmove(R_str, R_str, R_str.find("*")):
+;
+    else
+        R = atof(R):
     if (r < (R / 2))
-        printf("Квадрат внутри круга, но не вписан");
+        printf("Круг внутри квадрата, но не вписан");
     else if (r >= (R / 2) && r <= (d / 2))
         if (r == R / 2)
             printf("Круг вписан в квадрат");
