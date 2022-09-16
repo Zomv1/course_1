ea6243de4a22a61c51a99088c52a8d1fecc4dd6e
AMZomb <50305192+AMZomb@users.noreply.github.com>
2022-09-16 09:47:52 +0300

    Update task_1.c

--- task_1.c
+++ task_1.c
@@ -4,25 +4,29 @@
 
 int main(int argc, char* arv[])
 {
-    double r,R, d;
-    printf("Введите длину стороны квадрата\n");
+    double r,R,d;
+    printf("Введите длину стороны квадрата - ");
     scanf("%lf", &R);
-    printf("\nВведите радиус круга\n");
+    printf("\nВведите радиус круга - ");
     scanf("%lf", &r);
+    d = round(R*sqrt(2));
+    printf("\nd =%lf", d);
+    printf("\nd/2 =%lf", d/2);
+    printf("\nr =%lf", r);
+    printf("\nR =%lf", R);
+    printf("\n\n");
     
-    d = R*sqrt(2);
-    if(r > (R/2))
-        if(r < (d/2))
-            printf("\n\nКруг и квадрат касаются, но не вписаны");
-        else
-            printf("\n\nКвадрат внутри круга, но не вписан");
+
+    if(r < (R/2))
+        printf("Квадрат внутри круга, но не вписан");
+    else if(r >= (R/2) && r <= (d/2))
+        if(r == R/2)
+            printf("Круг вписан в квадрат");
+        else if(r == (d/2))
+            printf("Квадрат вписан в круг");
+        else 
+            printf("Квадрат и круг касаются, но не пересекаются");
     else
-        if(r == (R/2))
-            printf("\n\nКруг вписан в квадрат");
-        else
-            if(r == (d/2))
-                printf("\n\nКвадрат вписан в круг");
-            else
-                printf("\n\nКруг в квадрате, но не вписан");
+        printf("Квадрат в круге, но не вписан");
     return 0;
 }
