27c67ee9951711cf3aaa49d5e27225b6e8300f2a
AMZomb <50305192+AMZomb@users.noreply.github.com>
2022-09-17 12:27:28 +0300

    Update task_2.c

--- task_2.c
+++ task_2.c
@@ -1,45 +1,79 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-int main(int argc, char* arv[])
+#include <windows.h>
+int main()
 {
+    system("chcp 1251");
     double l,w,h,a,b;
     printf("Введите длину шкафа - ");
-    scanf("%lf", &w);
+    scanf("%lf", w);
     printf("\nВведите ширину шкафа -");
-    scanf("%lf", &l);
+    scanf("%lf", l);
     printf("\nВведите высоту шкафа -");
-    scanf("%lf", &h);
+    scanf("%lf", h);
     printf("\nВведите высоту двери -");
-    scanf("%lf", &a);
+    scanf("%lf", x);
     printf("\nВведите ширину двери -");
-    scanf("%lf", &b);
+    scanf("%lf", y);
     printf("\n\n");
-    if(l <= a)
-        if(w <= b)
-            printf("Пройдет верхней или нижней стороной");
-        else
-            if(h <= b)
-                if(w <= a)
-                    printf("Пройдет тыльной или задней стороной");
-                else
-                    printf("Пройдет верхней или нижней стороной");
-            else
+
+
+    // РЕШЕНИЕ 1
+    if(h <= x) {
+        if(w <= y) {
+            printf("Пройдет тыльной или задней стороной");
+        }
+        else if(l <= y) {
+            printf("Пройдет боковой стороной");
+        }
+    }
+    else if(h <= y) {
+        if(w <= x) {
+            printf("Пройдет тыльной или задней стороной");
+        }
+        else if(l <= x) {
+            printf("Пройдет боковой стороной");
+        }
+    }
+    else {
+        if(w <= x) {
+            if(l <= y) {
+                printf("Пройдет верхней или нижней стороной");
+            }
+            else {
                 printf("Не пройдет");
-    else
-        if(l<=b)
-            if(w <= a)
-                ("Пройдет верхней или нижней стороной")
-            else
-                if(h <= a)
-                    if(w <= b)
-                        printf("Пройдет боком");
-                    else
-                        printf("Пройдет тыльной или задней стороной");
-                else
-                    printf("Не пройдет")
-        else
+            }
+        }
+        else if(w <= y) {
+            if(l <= x) {
+                printf("Пройдет верхней или нижней стороной");
+            }
+            else {
+                printf("Не пройдет");
+            }
+        }
+        else {
             printf("Не пройдет");
+        }
+    }
+
+
+    // РЕШЕНИЕ 2
+    if((h <= x && w <= y) || (w <= x && h <= y)) {
+        printf("Пройдет тыльной или задней стороной");
+    }
+    else if((h <= x && l <= y) || (l <= x && h <= y)) {
+        printf("Пройдет боковой стороной");
+    }
+    else if((w <= x && l <= y) || (l <= x && w <= y)) {
+        printf("Пройдет верхней или нижней стороной");
+    }
+    else {
+        printf("Не пройдет");
+    }
+
+    
+    system("pause");
     return 0;
 }
