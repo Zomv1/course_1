096c50d8420a290c81a35b6039d3cf17d48a52c1
Pavel Kulikov <50305192+Zomv1@users.noreply.github.com>
2022-12-10 05:13:56 +0300

    Update lab3.c

--- lab3.c
+++ lab3.c
@@ -9,7 +9,7 @@ int main()
 	int random_num = rand() % 1000, guess;
 	printf("Введите Вашу догадку(загаданное число в диапазоне 0-1000):");
 	do{
-		scanf("%i", &guess);
+		scanf_s("%i", &guess);
 		if(guess < random_num) printf("\n Ваше число меньше, введите новое: \n");
 		else if(guess > random_num) printf("\n Ваше число больше, введите новое: \n");
 		else printf("\nВы угадали!");
